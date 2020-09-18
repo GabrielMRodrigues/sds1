80172903ec6120f92f0c0e40fd47bd503660b4cd
Gabriel Rodrigues <gabrielm1212@gmail.com>
2020-09-17 21:58:45 -0300

    POST records

--- backend/src/main/java/com/devsuperior/dspesquisa/resources/GameResource.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/GameResource.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dspesquisa.dto.GameDTO;
+import com.devsuperior.dspesquisa.entities.Game;
 import com.devsuperior.dspesquisa.services.GameService;
 
 @RestController
@@ -18,7 +19,7 @@ public class GameResource {
 	@Autowired
 	private GameService service;
 
-	@GetMapping
+	@PostMapping
 	public ResponseEntity<List<GameDTO>> findAll() {
 		List<GameDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
